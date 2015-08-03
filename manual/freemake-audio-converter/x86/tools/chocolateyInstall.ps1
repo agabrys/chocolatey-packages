4067c26c3c19821c1bf4fd114bc6cd6e81a7a5d4
agabrys <adam.gabrys@live.com>
2015-08-03 19:17:20 +0200

    Add Freemake Audio Converter.

---
+++ manual/freemake-audio-converter/x86/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+$packageArgs = @{
+  packageName    = 'freemake-audio-converter'
+  installerType  = 'exe'
+  silentArgs     = '/VERYSILENT /NORESTART'
+  url            = 'http://download.freemake.net/FreemakeOriginals2/FreemakeAudioConverterSetup.exe'
+  checksum       = '3c6ab60196c6f2089db468e1e7098dec'
+  checksumType   = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
