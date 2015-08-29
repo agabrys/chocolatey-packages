df50fa1599c8f10d45728db68284aceb8281a212
agabrys <adam.gabrys@live.com>
2015-08-29 16:13:24 +0200

    freemake-audio-converter: remove checksum (file can be dynamically changed)

--- manual/freemake-audio-converter/x86/tools/chocolateyInstall.ps1
+++ manual/freemake-audio-converter/x86/tools/chocolateyInstall.ps1
@@ -3,8 +3,6 @@ $packageArgs = @{
   installerType = 'exe'
   silentArgs    = '/VERYSILENT /NORESTART'
   url           = 'http://download.freemake.net/FreemakeOriginals2/FreemakeAudioConverterSetup.exe'
-  checksum      = '3c6ab60196c6f2089db468e1e7098dec'
-  checksumType  = 'md5'
 }
 
 Install-ChocolateyPackage @packageArgs
