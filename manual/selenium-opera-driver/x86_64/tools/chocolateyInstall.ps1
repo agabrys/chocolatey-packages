85d6d9d76931b109b8d159f3f1417ad1bda8b33f
agabrys <adam.gabrys@live.com>
2016-04-03 20:59:59 +0200

    selenium-opera-driver: create 0.2.2 package

---
+++ manual/selenium-opera-driver/x86_64/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$tmpDir = "$toolsDir\temp"
+
+$packageArgs = @{
+  packageName   = 'selenium-opera-driver'
+  url           = 'https://github.com/operasoftware/operachromiumdriver/releases/download/v0.2.2/operadriver_win32.zip'
+  checksum      = 'c0612082fd2d174e6bdcf2a718f8ad60'
+  checksumType  = 'md5'
+  url64bit       = 'https://github.com/operasoftware/operachromiumdriver/releases/download/v0.2.2/operadriver_win64.zip'
+  checksum64     = 'f7b02eaae2e87ec810f7958ec2518a40'
+  checksumType64 = 'md5'
+  unzipLocation  = $tmpDir	
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Move-Item $tmpDir\operadriver.exe $toolsDir\driver.exe -Force
+Remove-Item $tmpDir -Recurse -Force
