e38f793af93cf30ba14adfc12376e66b42bcfc26
agabrys <adam.gabrys@live.com>
2016-04-03 23:26:00 +0200

    selenium-opera-driver: add shortcut

--- manual/selenium-opera-driver/x86_64/tools/chocolateyInstall.ps1
+++ manual/selenium-opera-driver/x86_64/tools/chocolateyInstall.ps1
@@ -16,3 +16,12 @@ Install-ChocolateyZipPackage @packageArgs
 New-Item $toolsDir\bin -ItemType directory
 Move-Item $tmpDir\operadriver.exe $toolsDir\bin\driver.exe -Force
 Remove-Item $tmpDir -Recurse -Force
+
+$menuPrograms = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutArgs = @{
+  shortcutFilePath = "$menuPrograms\Selenium\Selenium Opera Driver.lnk"
+  targetPath       = "$toolsDir\bin\driver.exe"
+  iconLocation     = "$toolsDir\icon.ico"
+}
+
+Install-ChocolateyShortcut @shortcutArgs
--- manual/selenium-opera-driver/x86_64/tools/chocolateyUninstall.ps1
+++ manual/selenium-opera-driver/x86_64/tools/chocolateyUninstall.ps1
@@ -1,1 +1,6 @@
-# FYI - all files are located in internal package directory and will be deleted by standard 'choco uninstall selenium-opera-driver' command
+$menuPrograms = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = "$menuPrograms\Selenium\Selenium Opera Driver.lnk"
+
+If (Test-Path $shortcutFilePath) {
+  Remove-Item $shortcutFilePath
+}
