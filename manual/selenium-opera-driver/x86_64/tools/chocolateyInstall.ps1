c2675e16323965e822afbfb7a8ef2631daeedfb9
agabrys <adam.gabrys@live.com>
2016-04-04 20:02:54 +0200

    selenium-opera-driver: reorganize package

--- manual/selenium-opera-driver/x86_64/tools/chocolateyInstall.ps1
+++ manual/selenium-opera-driver/x86_64/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $tmpDir = "$toolsDir\temp"
 
 $packageArgs = @{
-  packageName   = 'selenium-opera-driver'
-  url           = 'https://github.com/operasoftware/operachromiumdriver/releases/download/v0.2.2/operadriver_win32.zip'
-  checksum      = 'c0612082fd2d174e6bdcf2a718f8ad60'
-  checksumType  = 'md5'
+  packageName    = 'selenium-opera-driver'
+  url            = 'https://github.com/operasoftware/operachromiumdriver/releases/download/v0.2.2/operadriver_win32.zip'
+  checksum       = 'c0612082fd2d174e6bdcf2a718f8ad60'
+  checksumType   = 'md5'
   url64bit       = 'https://github.com/operasoftware/operachromiumdriver/releases/download/v0.2.2/operadriver_win64.zip'
   checksum64     = 'f7b02eaae2e87ec810f7958ec2518a40'
   checksumType64 = 'md5'
@@ -13,14 +13,21 @@ $packageArgs = @{
 }
 
 Install-ChocolateyZipPackage @packageArgs
-New-Item $toolsDir\bin -ItemType directory
-Move-Item $tmpDir\operadriver.exe $toolsDir\bin\driver.exe -Force
+
+$binRoot = Get-BinRoot
+$seleniumDir = "$binRoot\selenium"
+$driverPath = "$seleniumDir\opera-driver.exe"
+
+If (!(Test-Path -Path $seleniumDir)) {
+  New-Item $seleniumDir -ItemType directory
+}
+Move-Item $tmpDir\operadriver.exe $driverPath -Force
 Remove-Item $tmpDir -Recurse -Force
 
 $menuPrograms = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutArgs = @{
   shortcutFilePath = "$menuPrograms\Selenium\Selenium Opera Driver.lnk"
-  targetPath       = "$toolsDir\bin\driver.exe"
+  targetPath       = $driverPath
   iconLocation     = "$toolsDir\icon.ico"
 }
 
