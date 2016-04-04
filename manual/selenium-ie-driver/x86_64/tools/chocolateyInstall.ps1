0fc52994a4f2fe1a7dfae74aadb147f6156a64b7
agabrys <adam.gabrys@live.com>
2016-04-04 20:02:33 +0200

    selenium-ie-driver: formatting

--- manual/selenium-ie-driver/x86_64/tools/chocolateyInstall.ps1
+++ manual/selenium-ie-driver/x86_64/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $tmpDir = "$toolsDir\temp"
 
 $packageArgs = @{
-  packageName   = 'selenium-ie-driver'
-  url           = 'http://selenium-release.storage.googleapis.com/2.53/IEDriverServer_Win32_2.53.0.zip'
-  checksum      = '63066830741d59ae6c74045a9f24291c'
-  checksumType  = 'md5'
+  packageName    = 'selenium-ie-driver'
+  url            = 'http://selenium-release.storage.googleapis.com/2.53/IEDriverServer_Win32_2.53.0.zip'
+  checksum       = '63066830741d59ae6c74045a9f24291c'
+  checksumType   = 'md5'
   url64bit       = 'http://selenium-release.storage.googleapis.com/2.53/IEDriverServer_x64_2.53.0.zip'
   checksum64     = '0acce304cbd05a25ba6f11f7c8b9311c'
   checksumType64 = 'md5'
