a4107c541485d70d1d703f5eb6957674d93f0bec
agabrys <adam.gabrys@live.com>
2016-08-10 23:14:13 +0200

    selenium-opera-driver: correct formatting

--- manual/selenium-opera-driver/x86_64/tools/chocolateyInstall.ps1
+++ manual/selenium-opera-driver/x86_64/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   url64bit       = 'https://github.com/operasoftware/operachromiumdriver/releases/download/v0.2.2/operadriver_win64.zip'
   checksum64     = 'f7b02eaae2e87ec810f7958ec2518a40'
   checksumType64 = 'md5'
-  unzipLocation  = $tmpDir	
+  unzipLocation  = $tmpDir
 }
 
 Install-ChocolateyZipPackage @packageArgs
