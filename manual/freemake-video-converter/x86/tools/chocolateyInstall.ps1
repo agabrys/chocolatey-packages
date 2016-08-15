047772132ffdf748d25538ce44f449d66988b9f6
agabrys <adam.gabrys@live.com>
2016-08-15 08:00:09 +0200

    #6 #2 freemake-video-converter: upgrade to 4.1.9.31 + add checksum and download full installer

--- manual/freemake-video-converter/x86/tools/chocolateyInstall.ps1
+++ manual/freemake-video-converter/x86/tools/chocolateyInstall.ps1
@@ -2,7 +2,9 @@ $packageArgs = @{
   packageName   = 'freemake-video-converter'
   installerType = 'exe'
   silentArgs    = '/VERYSILENT /NORESTART'
-  url           = 'http://download.freemake.net/FreemakeOriginals2/FreemakeVideoConverterSetup.exe'
+  url           = 'http://packages.chocolatey.adam.gabrys.biz/freemake-video-converter/freemake-video-converter-4.1.9.31.exe'
+  checksum      = '219dc1d9aeb09d4886308cc5779b7c10'
+  checksumType  = 'md5'
 }
 
 Install-ChocolateyPackage @packageArgs
