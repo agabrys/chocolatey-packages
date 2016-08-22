def87dab35a0c69aa579cec473e44623883475bf
agabrys <adam.gabrys@live.com>
2016-08-22 21:54:00 +0200

    #5 freemake-audio-converter: upgrade to 1.1.8.4

--- manual/freemake-audio-converter/x86/tools/chocolateyInstall.ps1
+++ manual/freemake-audio-converter/x86/tools/chocolateyInstall.ps1
@@ -2,7 +2,9 @@ $packageArgs = @{
   packageName   = 'freemake-audio-converter'
   installerType = 'exe'
   silentArgs    = '/VERYSILENT /NORESTART'
-  url           = 'http://download.freemake.net/FreemakeOriginals2/FreemakeAudioConverterSetup.exe'
+  url           = 'http://packages.chocolatey.adam.gabrys.biz/freemake-audio-converter/freemake-audio-converter-1.1.8.4.exe'
+  checksum      = 'fa9dd7ab5a03ebd14324d72f128e1585'
+  checksumType  = 'md5'
 }
 
 Install-ChocolateyPackage @packageArgs
