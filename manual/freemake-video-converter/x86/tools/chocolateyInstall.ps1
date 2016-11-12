2b2b1e9ffb7acb493fdc32186af1b3dbfdd8c9fd
agabrys <adam.gabrys@live.com>
2016-11-12 12:08:07 +0100

    freemake-video-converter: upgrade to 4.1.9.45

--- manual/freemake-video-converter/x86/tools/chocolateyInstall.ps1
+++ manual/freemake-video-converter/x86/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@ $packageArgs = @{
   packageName   = 'freemake-video-converter'
   installerType = 'exe'
   silentArgs    = '/VERYSILENT /NORESTART'
-  url           = 'http://packages.chocolatey.adam.gabrys.biz/freemake-video-converter/freemake-video-converter-4.1.9.42.exe'
-  checksum      = 'e535f2e3f7a6b41bc3b350d2edea380a'
+  url           = 'http://packages.chocolatey.adam.gabrys.biz/freemake-video-converter/freemake-video-converter-4.1.9.45.exe'
+  checksum      = '77338f53643e40e864da5539b6629b8d'
   checksumType  = 'md5'
 }
 
