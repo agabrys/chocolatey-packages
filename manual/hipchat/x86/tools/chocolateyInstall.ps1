d1e8d79b346cd170c55bf3c690bb15d184458002
agabrys <adam.gabrys@live.com>
2016-12-11 12:40:27 +0100

    hipchat: upgrade to 4.29.5.1662

--- manual/hipchat/x86/tools/chocolateyInstall.ps1
+++ manual/hipchat/x86/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@ $packageArgs = @{
   packageName   = 'hipchat'
   installerType = 'exe'
   silentArgs    = '/VERYSILENT /NORESTART /NOCLOSEAPPLICATIONS /NORESTARTAPPLICATIONS'
-  url           = 'https://hipchat-ops.s3.amazonaws.com/hipchat4/windows/HipChat-4.29.4.1661.exe'
-  checksum      = '5f75b0a6663cc84590bf392159928c59'
+  url           = 'https://hipchat-ops.s3.amazonaws.com/hipchat4/windows/HipChat-4.29.5.1662.exe'
+  checksum      = 'e02913c2ec8b679ccacad4d34c75805e'
   checksumType  = 'md5'
 }
 
