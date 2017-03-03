0f3357637b7def7d61f746fcabd9df730bb01cdb
agabrys <adam.gabrys@live.com>
2017-03-03 15:19:35 +0100

    burp-suite-free-edition: upgrade to 1.7.19

--- manual/burp-suite-free-edition/noarch/tools/chocolateyInstall.ps1
+++ manual/burp-suite-free-edition/noarch/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $appDir = "$toolsDir\app"
 $packageArgs = @{
   packageName  = 'burp-suite-free-edition'
   fileFullPath = "$appDir\burpsuite.jar"
-  url          = 'https://portswigger.net/Burp/Releases/Download?productId=100&version=1.7.18&type=Jar'
-  checksum     = '95ba0c1cb3b0de11a0bbc391bdb565bf'
+  url          = 'https://portswigger.net/Burp/Releases/Download?productId=100&version=1.7.19&type=Jar'
+  checksum     = 'bfb0dfb33b175b2eefc96694c1a635b4'
   checksumType = 'md5'
 }
 
