84c6f307eb0e234d12b4477a7773e8699defe193
agabrys <adam.gabrys@live.com>
2017-07-12 12:55:19 +0200

    selenium-gecko-driver: upgrade to 0.18.0

--- manual/selenium-gecko-driver/x86_64/tools/chocolateyInstall.ps1
+++ manual/selenium-gecko-driver/x86_64/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@ $tmpDir = "$toolsDir\temp"
 
 $packageArgs = @{
   packageName    = 'selenium-gecko-driver'
-  url            = 'https://github.com/mozilla/geckodriver/releases/download/v0.17.0/geckodriver-v0.17.0-win32.zip'
-  checksum       = 'be8a441b3335c9162e65b0256e71b069'
-  checksumType   = 'md5'
-  url64bit       = 'https://github.com/mozilla/geckodriver/releases/download/v0.17.0/geckodriver-v0.17.0-win64.zip'
-  checksum64     = 'c9ff3b48287d983c4e368318ffd22897'
-  checksumType64 = 'md5'
+  url            = 'https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-win32.zip'
+  checksum       = '870faf980eeee77d7ce1f5686f9f821610b95002a8232037649215521ee8aeff7bb3d11ae65346d8996eda169e0f46b081b5e880ba3e6028eb710cd905e818d5'
+  checksumType   = 'sha512'
+  url64bit       = 'https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-win64.zip'
+  checksum64     = '743d0ec78e81975bc434e4b0017c4916acaa35229eb81388b7c611eb9502f8be71ad325564672b021c7829730e1541992dc90fc03763155ccb6bb050d4f7bd60'
+  checksumType64 = 'sha512'
   unzipLocation  = $tmpDir
 }
 
