563b067e1a53eb008ce5810d193f31e177307173
agabrys <adam.gabrys@live.com>
2017-08-17 17:28:24 +0200

    selenium-ie-driver: upgrade to 3.5.0

--- manual/selenium-ie-driver/x86_64/tools/chocolateyInstall.ps1
+++ manual/selenium-ie-driver/x86_64/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@ $tmpDir = "$toolsDir\temp"
 
 $packageArgs = @{
   packageName    = 'selenium-ie-driver'
-  url            = 'https://selenium-release.storage.googleapis.com/3.4/IEDriverServer_Win32_3.4.0.zip'
-  checksum       = 'e63776eb2765f2544ff59d65bc7bf2b4'
-  checksumType   = 'md5'
-  url64bit       = 'https://selenium-release.storage.googleapis.com/3.4/IEDriverServer_x64_3.4.0.zip'
-  checksum64     = '62da5a1dad57eb7b8257a8b6229481fa'
-  checksumType64 = 'md5'
+  url            = 'https://selenium-release.storage.googleapis.com/3.5/IEDriverServer_Win32_3.5.0.zip'
+  checksum       = 'a1db2b475c389b2dbb491a7174eaf05ab67bdcbf9866d847f4dc519564d336c67d774e85abb35dee81a73a144f68333c6bc5f612396ea71304cbde37c097ff07'
+  checksumType   = 'sha512'
+  url64bit       = 'https://selenium-release.storage.googleapis.com/3.5/IEDriverServer_x64_3.5.0.zip'
+  checksum64     = '290c7d3d861e0c305496ff69af6f3e9b960126c72393065aa725208f1c35cd661ed1f2ba95620d247ca2d687d5224d389723df9b0863d62ee58d73a66aea31f6'
+  checksumType64 = 'sha512'
   unzipLocation  = $tmpDir
 }
 
