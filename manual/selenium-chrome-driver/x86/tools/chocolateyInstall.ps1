a28d2ce21bd677ab1318d7efbb25bccadf5fddec
agabrys <adam.gabrys@live.com>
2017-11-03 18:30:57 +0100

    #17 selenium-chrome-driver: add shim support and stop removal of the driver file with non-default name

--- manual/selenium-chrome-driver/x86/tools/chocolateyInstall.ps1
+++ manual/selenium-chrome-driver/x86/tools/chocolateyInstall.ps1
@@ -1,31 +1,18 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$tmpDir = "$toolsDir\temp"
+$toolsLocation = Get-ToolsLocation
+$seleniumDir = "$toolsLocation\selenium"
+$driverPath = "$seleniumDir\chromedriver.exe"
 
 $packageArgs = @{
   packageName   = 'selenium-chrome-driver'
   url           = 'https://chromedriver.storage.googleapis.com/2.33/chromedriver_win32.zip'
   checksum      = 'cbe079e82756b207faef5d592d328f3b46aa36b8d5caabe0bbac5acecce208d4aef42d68a30ec23a873c5bab398618145c8b6fcfd9bcd0a6f93cb1c70b3320d5'
   checksumType  = 'sha512'
-  unzipLocation = $tmpDir
+  unzipLocation = $seleniumDir
 }
-
 Install-ChocolateyZipPackage @packageArgs
 
-$toolsLocation = Get-ToolsLocation
-$seleniumDir = "$toolsLocation\selenium"
-$driverPath = "$seleniumDir\chromedriver.exe"
-
-If (!(Test-Path $seleniumDir)) {
-  New-Item $seleniumDir -ItemType directory
-}
-Move-Item $tmpDir\chromedriver.exe $driverPath -Force
-Write-Host -ForegroundColor Green Moved driver to $seleniumDir
-Remove-Item $tmpDir -Recurse -Force
-
-$oldDriverPath = "$seleniumDir\chrome-driver.exe"
-If (Test-Path $oldDriverPath) {
-  Remove-Item $oldDriverPath -Force
-}
+Install-BinFile -Name 'chromedriver' -Path $driverPath
 
 $menuPrograms = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutArgs = @{
@@ -33,5 +20,4 @@ $shortcutArgs = @{
   targetPath       = $driverPath
   iconLocation     = "$toolsDir\icon.ico"
 }
-
 Install-ChocolateyShortcut @shortcutArgs
