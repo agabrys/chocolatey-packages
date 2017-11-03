ccc2cb7f3be382fca4e0e5fd001999effef74b42
agabrys <adam.gabrys@live.com>
2017-11-03 21:15:32 +0100

    #17 selenium-edge-driver: add shim support and stop removal of the driver file with non-default name

--- manual/selenium-edge-driver/x86/tools/chocolateyInstall.ps1
+++ manual/selenium-edge-driver/x86/tools/chocolateyInstall.ps1
@@ -1,26 +1,18 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$tmpDir = "$toolsDir\temp"
+$toolsLocation = Get-ToolsLocation
+$seleniumDir = "$toolsLocation\selenium"
+$driverPath = "$seleniumDir\MicrosoftWebDriver.exe"
 
 $packageArgs = @{
   packageName  = 'selenium-edge-driver'
   url          = 'https://download.microsoft.com/download/D/4/1/D417998A-58EE-4EFE-A7CC-39EF9E020768/MicrosoftWebDriver.exe'
   checksum     = '11eff15b47d417f3831ee1ef78f5d0907f1a29fc5d3fc4dd6fde28bc94369fdf98434e03489398d02fa616c4246e171ba1667f5b164a86df695fea854a852857'
   checksumType = 'sha512'
-  fileFullPath = "$tmpDir/MicrosoftWebDriver.exe"
+  fileFullPath = $driverPath
 }
-
 Get-ChocolateyWebFile @packageArgs
 
-$toolsLocation = Get-ToolsLocation
-$seleniumDir = "$toolsLocation\selenium"
-$driverPath = "$seleniumDir\MicrosoftWebDriver.exe"
-
-If (!(Test-Path $seleniumDir)) {
-  New-Item $seleniumDir -ItemType directory
-}
-Move-Item $tmpDir\MicrosoftWebDriver.exe $driverPath -Force
-Write-Host -ForegroundColor Green Moved driver to $seleniumDir
-Remove-Item $tmpDir -Recurse -Force
+Install-BinFile -Name 'MicrosoftWebDriver' -Path $driverPath
 
 $menuPrograms = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutArgs = @{
@@ -28,5 +20,4 @@ $shortcutArgs = @{
   targetPath       = $driverPath
   iconLocation     = "$toolsDir\icon.ico"
 }
-
 Install-ChocolateyShortcut @shortcutArgs
