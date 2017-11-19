87eeddfb65a42247eeb36c3f243880efa5b7551b
agabrys <adam.gabrys@live.com>
2017-11-19 11:15:53 +0100

    burp-suite-free-edition: upgrade to 1.7.28

--- manual/burp-suite-free-edition/noarch/tools/chocolateyInstall.ps1
+++ manual/burp-suite-free-edition/noarch/tools/chocolateyInstall.ps1
@@ -4,15 +4,15 @@ $appDir = "$toolsDir\app"
 $packageArgs = @{
   packageName  = 'burp-suite-free-edition'
   fileFullPath = "$appDir\burpsuite.jar"
-  url          = 'https://portswigger.net/Burp/Releases/Download?productId=100&version=1.7.27&type=Jar'
-  checksum     = '17f39e21bf0039480c27659ef775f144d446078a954776dc753791157132502003dceef878e0c92f0471bb698701c78df5e25ed4ba2e570a40105b5583f090c4'
+  url          = 'https://portswigger.net/Burp/Releases/Download?productId=100&version=1.7.28&type=Jar'
+  checksum     = '6cd5ec45dc9f83d05734d1b7125012c73b3a2f1b8d022788422aefee11ccb3777e7194674e2d953e24fdf8ef17ddbb10f06a76f7df6462154269de0e979f175f'
   checksumType = 'sha512'
 }
 
 Get-ChocolateyWebFile @packageArgs
 
 $menuPrograms = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-$wshShell = New-Object -comObject WScript.Shell
+$wshShell = New-Object -ComObject WScript.Shell
 $shortcut = $wshShell.CreateShortcut("$menuPrograms\Burp Suite Free Edition.lnk")
 $shortcut.TargetPath = "javaw.exe"
 $Shortcut.Arguments = "-jar `"$appDir\burpsuite.jar`""
--- manual/burp-suite-free-edition/noarch/tools/chocolateyUninstall.ps1
+++ manual/burp-suite-free-edition/noarch/tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 $menuPrograms = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = "$menuPrograms\Burp Suite Free Edition.lnk"
 
-If (Test-Path $shortcutFilePath) {
-  Remove-Item $shortcutFilePath
+If (Test-Path -Path $shortcutFilePath) {
+  Remove-Item -Path $shortcutFilePath
 }
