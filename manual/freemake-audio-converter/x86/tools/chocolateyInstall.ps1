d4b1046b41a7d9cc1e5a103655e1136f97886c55
agabrys <adam.gabrys@live.com>
2017-12-25 11:50:27 +0100

    freemake-audio-converter: revert to 1.1.8.10

--- manual/freemake-audio-converter/x86/tools/chocolateyInstall.ps1
+++ manual/freemake-audio-converter/x86/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@ $packageArgs = @{
   packageName   = 'freemake-audio-converter'
   installerType = 'exe'
   silentArgs    = '/VERYSILENT /NORESTART'
-  url           = 'http://packages.chocolatey.adam.gabrys.biz/freemake-audio-converter/freemake-audio-converter-1.1.8.12.exe'
-  checksum      = '38ee1c376a63389fd535afd1b6a4829e4f874aa29ea550274ead020db1bcf1ad6a61b91abdecda6da4bc694981691087c77123ae74ec6f35687dda9a3b90fe95'
+  url           = 'http://packages.chocolatey.adam.gabrys.biz/freemake-audio-converter/freemake-audio-converter-1.1.8.10.exe'
+  checksum      = '948a0219081ece52000d178d68bb5bce98b3608edd8e15db4fa893a3f7383b7250918c9c631942955870e4161edd64ff59831cf74caa049b1bb7d8f7cef16752'
   checksumType  = 'sha512'
 }
 Install-ChocolateyPackage @packageArgs
