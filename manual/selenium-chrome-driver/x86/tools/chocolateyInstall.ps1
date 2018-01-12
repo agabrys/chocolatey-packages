94a5e07c44818e835d227912c08e38b788f52b79
agabrys <adam.gabrys@live.com>
2018-01-12 20:41:17 +0100

    selenium-chrome-driver: upgrade to 2.35

--- manual/selenium-chrome-driver/x86/tools/chocolateyInstall.ps1
+++ manual/selenium-chrome-driver/x86/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $driverPath = "$seleniumDir\chromedriver.exe"
 
 $packageArgs = @{
   packageName   = 'selenium-chrome-driver'
-  url           = 'https://chromedriver.storage.googleapis.com/2.34/chromedriver_win32.zip'
-  checksum      = '8e7e2e2fc2961e72ec7021b6f57508d2d89f00d3e8940ab0943f5c36865728f27feecbbb6bda003fe056bc632fe9c99d8b1a933b6a47e8b6b8c2155393932c7e'
+  url           = 'https://chromedriver.storage.googleapis.com/2.35/chromedriver_win32.zip'
+  checksum      = '27b155a42ef8b8c4dc46313173eb156005bb428eb582461cb79bd9aabafe240a2812723a5183ef126ce4c90e7aa7e01fffe759c744c097c2ed68b20242bcc30d'
   checksumType  = 'sha512'
   unzipLocation = $seleniumDir
 }
