7e522b0471784e38aa05184fe405d6952fa3cd01
agabrys <adam.gabrys@live.com>
2018-06-30 16:20:43 +0200

    selenium-ie-driver: upgrade to 3.13.0

--- manual/selenium-ie-driver/x86_64/tools/chocolateyInstall.ps1
+++ manual/selenium-ie-driver/x86_64/tools/chocolateyInstall.ps1
@@ -7,11 +7,11 @@ $parameters = Get-PackageParameters
 
 $packageArgs = @{
   packageName    = 'selenium-ie-driver'
-  url            = 'https://selenium-release.storage.googleapis.com/3.12/IEDriverServer_Win32_3.12.0.zip'
-  checksum       = '1b422a41a935769a7bea9ea8897157d8b214d7dc7e43a20dec443beacc55a3ca0b1178fab1b80435d4b09957e92151e3c046155301f7dacbecd91ae33934f7d7'
+  url            = 'https://selenium-release.storage.googleapis.com/3.13/IEDriverServer_Win32_3.13.0.zip'
+  checksum       = '953310fd6ef0904ccfb4ed6fbdc313a14af2ff175961117519ed253f7ec72170bf3a4a646e8b4c7b73e151924ffebdd6be93e7a712f5b2f804e183f69d2a4e92'
   checksumType   = 'sha512'
-  url64bit       = 'https://selenium-release.storage.googleapis.com/3.12/IEDriverServer_x64_3.12.0.zip'
-  checksum64     = 'f117a1eb8aaa00babc2caea35c776dea6733100bc764bfaa3e930a44fde0185bd943957ec16e73ec4e9deb4194b78c0787f91a2cb6be75f6ad815711cd13287d'
+  url64bit       = 'https://selenium-release.storage.googleapis.com/3.13/IEDriverServer_x64_3.13.0.zip'
+  checksum64     = '960346f0e6968958ed851d43d7cbbb401fe571dc5baf35ae78c18c6d1d3e87cda070e5f6d4c43b2f267836b8e97381476415793ab3159d80df7fada68d0fa5b9'
   checksumType64 = 'sha512'
   unzipLocation  = $seleniumDir
 }
