84b84c87e258183d66b5b7e8a0c025bc7b320339
agabrys <adam.gabrys@live.com>
2018-06-30 16:03:08 +0200

    burp-suite-free-edition: upgrade to 1.7.35

--- manual/burp-suite-free-edition/noarch/tools/chocolateyInstall.ps1
+++ manual/burp-suite-free-edition/noarch/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $appDir = "$toolsDir\app"
 $packageArgs = @{
   packageName  = 'burp-suite-free-edition'
   fileFullPath = "$appDir\burpsuite.jar"
-  url          = 'https://portswigger.net/Burp/Releases/Download?productId=100&version=1.7.34&type=Jar'
-  checksum     = 'd2f1e60ef4072764a396d7588ca207a1fb4427844310c1d8c5abdf60d5fd10d2f206461a42b73e7169975d7f8dcffdfe46dc689e07d1c08915dcc57db716e30b'
+  url          = 'https://portswigger.net/Burp/Releases/Download?productId=100&version=1.7.35&type=Jar'
+  checksum     = '775a45872da1d514132f5bbcd8a2b58d15f0101b8d41bdfcdd216456ab99080846193b57ad1a95c98dae1eac3de16134ab0631651d44cd0c84788e8eeb4149e3'
   checksumType = 'sha512'
 }
 Get-ChocolateyWebFile @packageArgs
