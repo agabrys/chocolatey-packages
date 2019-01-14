22e640cec06570415f084d98501fd2cbbade3807
agabrys <adam.gabrys@live.com>
2019-01-14 23:09:14 +0100

    mucommander: upgrade to 0.9.3.3

--- manual/mucommander/x86/tools/chocolateyInstall.ps1
+++ manual/mucommander/x86/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@ $packageArgs = @{
   packageName   = 'mucommander'
   installerType = 'exe'
   silentArgs    = '/S'
-  url           = 'https://github.com/mucommander/mucommander/releases/download/0.9.3-2/mucommander-0.9.3.exe'
-  checksum      = '59f140c94e920659bfab8430ddbc6b186e3fd202dcab6346fd6327d25cc623ac072b3762fe414331dde719e4f2faec9009efddd2706257f9b446ffae52a71085'
+  url           = 'https://github.com/mucommander/mucommander/releases/download/0.9.3-3/mucommander-0.9.3.exe'
+  checksum      = 'be0b5529d0fac5ed1322982eca2a4d0ac2413fc21729b22ac009fb4e470d78bcb7ba9a32bfb0cfe9dfcb10d7ac8832aeaf0de114915e3301a0441e91e2017e48'
   checksumType  = 'sha512'
 }
 
