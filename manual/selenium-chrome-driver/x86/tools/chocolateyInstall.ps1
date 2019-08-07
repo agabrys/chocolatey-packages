cb5f3bd9b46943ca59c616dd6d83ad4bd786dccd
Adam Gabryś <adam.gabrys@sap.com>
2019-08-07 19:54:01 +0200

    selenium-chrome-driver: upgrade to 76.0.3809.25

--- manual/selenium-chrome-driver/x86/tools/chocolateyInstall.ps1
+++ manual/selenium-chrome-driver/x86/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $parameters = Get-PackageParameters
 
 $packageArgs = @{
   packageName   = 'selenium-chrome-driver'
-  url           = 'https://chromedriver.storage.googleapis.com/76.0.3809.12/chromedriver_win32.zip'
-  checksum      = 'f00e28a9bcc38ae4c133cad732677f95a21b7ed9564c03aff59656cdb5f34e594a7e83487482e87b33b95bd3903436d35aaa52bc88e620e7944b4d6c886a2b65'
+  url           = 'https://chromedriver.storage.googleapis.com/76.0.3809.25/chromedriver_win32.zip'
+  checksum      = 'b3ad663e8f2bf795c37bdf7d9fdb21a89b04f9d8c7999f986dae46a2ad5e0b2ae85f2fa14e9edfd36b8f396697a52fd49d4f741a34d8fd793daa2e3c243c5c5f'
   checksumType  = 'sha512'
   unzipLocation = $seleniumDir
 }
