6ebae65c9ea19a456127f8f1b21780c862ac7c3c
agabrys <adam.gabrys@live.com>
2019-09-29 14:26:42 +0200

    selenium-gecko-driver: upgrade to 0.25.0

--- manual/selenium-gecko-driver/x86_64/tools/chocolateyInstall.ps1
+++ manual/selenium-gecko-driver/x86_64/tools/chocolateyInstall.ps1
@@ -7,11 +7,11 @@ $parameters = Get-PackageParameters
 
 $packageArgs = @{
   packageName    = 'selenium-gecko-driver'
-  url            = 'https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-win32.zip'
-  checksum       = 'bdc1ac6d2d439422f5a88fb8b92d74133d1371edd2d8792b0d0ba86b995e85494b96f192c163aeb13d6cb9e3bee5cc4ed576b7435c7a65b77c3029ababc50f53'
+  url            = 'https://github.com/mozilla/geckodriver/releases/download/v0.25.0/geckodriver-v0.25.0-win32.zip'
+  checksum       = '151297af888e4c36e51457170f5f126f93c61761e5dd57d134ad1414147c175cca56a8fb6bb85a131f39f85cef92896b611d3127c26965a20d7d6833e4f4979f'
   checksumType   = 'sha512'
-  url64bit       = 'https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-win64.zip'
-  checksum64     = 'd1eb5263b70e65fd1dbb1255beb1c877a43facba310aaa451a59bb54c0025101235e0c00d024f58b79645302676ab8c01387e2d884dbf856a904ffa53688f481'
+  url64bit       = 'https://github.com/mozilla/geckodriver/releases/download/v0.25.0/geckodriver-v0.25.0-win64.zip'
+  checksum64     = '991079d8950a302c2ae6fac5336a28268bb81a4bd33eea86a682b567dee24a35201dd3b41a613c001fbf0c7920c0e9d11eda56a65f58c81f26139c7ada7bf436'
   checksumType64 = 'sha512'
   unzipLocation  = $seleniumDir
 }
