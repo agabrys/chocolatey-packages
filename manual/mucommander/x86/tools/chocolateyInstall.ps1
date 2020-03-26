899dd8658a165211a806d3d83381d97337c67b90
agabrys <adam.gabrys@live.com>
2020-03-26 19:58:48 +0100

    upgrade mucommander to 0.9.4.2

--- manual/mucommander/x86/tools/chocolateyInstall.ps1
+++ manual/mucommander/x86/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@ $packageArgs = @{
   packageName   = 'mucommander'
   installerType = 'exe'
   silentArgs    = '/S'
-  url           = 'https://github.com/mucommander/mucommander/releases/download/0.9.4/mucommander-0.9.4.exe'
-  checksum      = 'af5a2bba8a3f7df8e66f7b2b0d656cfe96eef6a885c1d65a3ff672cc6805ce2e5830bd9095e6e1dcbf1a639801ced3db18ef0d8795deaadfa84246ca32c8b877'
+  url           = 'https://github.com/mucommander/mucommander/releases/download/0.9.4-2/mucommander-0.9.4-2.exe'
+  checksum      = '66e7404f19f4b45cf7fb1c9833feb3a3c5666c1f46efbfc42913fcb301516e203511a37efe3cf483a910e27d3b8c31f177e4eb04550636f3fbe0b3ecfc114c58'
   checksumType  = 'sha512'
 }
 
