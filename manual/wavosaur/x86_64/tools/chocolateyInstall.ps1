bdc20a3eeda1eca2152f5bc16fd3366080e92c36
agabrys <adam.gabrys@live.com>
2020-03-26 20:13:05 +0100

    upgrade wavosaur to 1.4.0.0
    Installed the executable in the Get-ToolsLocation/wavosaur directory to fix broken Undo command.

--- manual/wavosaur/x86_64/tools/chocolateyInstall.ps1
+++ manual/wavosaur/x86_64/tools/chocolateyInstall.ps1
@@ -1,14 +1,19 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$appDir = "$toolsDir\app"
+$oldAppDir = "$toolsDir\app"
+If (Test-Path $oldAppDir) {
+  Remove-Item -Path $oldAppDir -Recurse -Force
+}
+
+$appDir = "$(Get-ToolsLocation)\wavosaur"
 
 $packageArgs = @{
   packageName    = 'wavosaur'
-  url            = 'https://www.wavosaur.com/download/files/Wavosaur.1.3.0.0-x86(en).zip'
-  checksum       = 'cf250930100f4e29d1cbb601567b5371'
-  checksumType   = 'md5'
-  url64bit       = 'https://www.wavosaur.com/download/files/Wavosaur.1.3.0.0-x64(en).zip'
-  checksum64     = '899d8ae578b3629ca05d7306a8c3cd85'
-  checksumType64 = 'md5'
+  url            = 'https://www.wavosaur.com/download/files/Wavosaur.1.4.0.0-x86.zip'
+  checksum       = '6ffdc58af985afc7b4ead991afebbd61ff84cd75a7fc5d49de3631b1ba4b46e3edb8bc732b2f3a0702882006de95b8a498bb8f70b578fecdab3d0486c6eaf744'
+  checksumType   = 'sha512'
+  url64bit       = 'https://www.wavosaur.com/download/files/Wavosaur.1.4.0.0-x64.zip'
+  checksum64     = '19a9ede07ac162e9ab09d5ba5cfaab470de3ef9ec6dc89308c3765e7b3518896196f476ba57b49382a003a8815526343b4a73436aabc77b96273413ebd5b21c5'
+  checksumType64 = 'sha512'
   unzipLocation  = $appDir
 }
 
--- manual/wavosaur/x86_64/tools/chocolateyUninstall.ps1
+++ manual/wavosaur/x86_64/tools/chocolateyUninstall.ps1
@@ -1,6 +1,10 @@
 $menuPrograms = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = "$menuPrograms\Wavosaur.lnk"
-
 If (Test-Path $shortcutFilePath) {
-  Remove-Item $shortcutFilePath
+  Remove-Item $shortcutFilePath -Force
+}
+
+$appDir = "$(Get-ToolsLocation)\wavosaur"
+If (Test-Path $appDir) {
+  Remove-Item -Path $appDir -Recurse -Force
 }
