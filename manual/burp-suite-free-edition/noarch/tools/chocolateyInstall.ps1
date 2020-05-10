8678ae645abbe4318316243f726c5116a53adaab
agabrys <adam.gabrys@live.com>
2020-05-10 17:09:34 +0200

    upgrade burp-suite-free-edition to 2020.4

--- manual/burp-suite-free-edition/noarch/tools/chocolateyInstall.ps1
+++ manual/burp-suite-free-edition/noarch/tools/chocolateyInstall.ps1
@@ -1,14 +1,15 @@
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $appDir = "$toolsDir\app"
 
 $packageArgs = @{
   packageName  = 'burp-suite-free-edition'
   fileFullPath = "$appDir\burpsuite.jar"
-  url          = 'https://portswigger.net/Burp/Releases/Download?productId=100&version=2020.2.1&type=jar'
-  checksum     = 'b33e10469200525edb41ec9d2a11f60ed88e57a463ebe01fc9aab6850fb90107fbc89493f89182ee44c7f32e27c7af494701f417182054a08b95118b36498181'
+  url          = 'https://portswigger.net/Burp/Releases/Download?productId=100&version=2020.4&type=jar'
+  checksum     = 'd22529fef8fdb809e0b1e96f439b159b9885ab0f211565422850d8576d4d3e8a9f6b8c9db430d4099aa907259a6d7f33c2399794ec46f974d220bee56251c5d8'
   checksumType = 'sha512'
 }
 Get-ChocolateyWebFile @packageArgs
+$env:ChocolateyPackageInstallLocation = $appDir
 
 $menuPrograms = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $wshShell = New-Object -ComObject WScript.Shell
