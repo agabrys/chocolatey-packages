68ea2925876d495c4d00ebb667f2809dbe062838
agabrys <adam.gabrys@live.com>
2020-05-31 19:20:02 +0200

    use JDK 13 in burp-suite-free-edition 2020.4.1

--- manual/burp-suite-free-edition/noarch/tools/chocolateyInstall.ps1
+++ manual/burp-suite-free-edition/noarch/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $appDir = "$toolsDir\app"
 $packageArgs = @{
   packageName  = 'burp-suite-free-edition'
   fileFullPath = "$appDir\burpsuite.jar"
-  url          = 'https://portswigger.net/Burp/Releases/Download?productId=100&version=2020.4&type=jar'
-  checksum     = 'd22529fef8fdb809e0b1e96f439b159b9885ab0f211565422850d8576d4d3e8a9f6b8c9db430d4099aa907259a6d7f33c2399794ec46f974d220bee56251c5d8'
+  url          = 'https://portswigger.net/Burp/Releases/Download?productId=100&version=2020.4.1&type=jar'
+  checksum     = '7ecad2f8fab846632164ed69ecdc0fff5118a305bdea86c30b203a70147e71711b446f59d9f8a5d235773d6c03ff6dfabba0a0d71ffceeaff3a424d24ad82992'
   checksumType = 'sha512'
 }
 Get-ChocolateyWebFile @packageArgs
