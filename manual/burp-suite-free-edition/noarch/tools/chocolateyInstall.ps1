dc5994edd6e0d96f96301678c9877b382997e3b5
agabrys <adam.gabrys@live.com>
2021-04-01 21:38:33 +0200

    update burp-suite-free-edition to 2021.3.1

--- manual/burp-suite-free-edition/noarch/tools/chocolateyInstall.ps1
+++ manual/burp-suite-free-edition/noarch/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $appDir = "${toolsDir}\app"
 $packageArgs = @{
   packageName  = 'burp-suite-free-edition'
   fileFullPath = "${appDir}\burpsuite.jar"
-  url          = 'https://portswigger.net/Burp/Releases/Download?productId=100&version=2021.3&type=jar'
-  checksum     = '73b86330fd03d7abcf6534887a2fec4f3c3d106e0aff4bb72d43b5fbeb52c08c57d6c05a5a6ba1b25dd080e34c9f846ec411780c65cee1762a86f67f25499341'
+  url          = 'https://portswigger.net/Burp/Releases/Download?productId=100&version=2021.3.1&type=jar'
+  checksum     = '6ff5c8bf1ba8038f427c24e13dce457de95945650664b7f1d232899b0e5e6b4a3d14c46db217194403a822da1a39d9f62575f7be204416ad4d29124e1ab51828'
   checksumType = 'sha512'
 }
 Get-ChocolateyWebFile @packageArgs
