226bdd5864bd21580844c60dd9eca1e76e96b755
agabrys <adam.gabrys@live.com>
2021-05-26 17:23:22 +0200

    update mucommander to 0.9.7

--- manual/mucommander/x86/tools/chocolateyInstall.ps1
+++ manual/mucommander/x86/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@ $packageArgs = @{
   packageName   = 'mucommander'
   installerType = 'exe'
   silentArgs    = '/S'
-  url           = 'https://github.com/mucommander/mucommander/releases/download/0.9.6-1/mucommander-0.9.6-1.exe'
-  checksum      = '9fa80be3c3b71b1a5329983c9318e84c3a7c2dc08c524116af15f1e7e819faf8bca3b2484c991d2b42d5ef399d26c5d465eb5877d353ab3695654df6a19eac76'
+  url           = 'https://github.com/mucommander/mucommander/releases/download/0.9.7-1/mucommander-0.9.7-1.exe'
+  checksum      = '8f159e5f3e23ea6305825303eb55538e39c9208d4a964ab3f191400549b7fb2fa49b2dddb974c1d8ef239fa2e15f11e1feae98902fa901fa212dd3aab7d83b83'
   checksumType  = 'sha512'
 }
 Install-ChocolateyPackage @packageArgs
