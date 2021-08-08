3cdc74f139ac22a501b40732cc0977b1db9e8bbe
agabrys <adam.gabrys@live.com>
2021-08-08 14:51:59 +0200

    update burp-suite-free-edition to 2021.8

--- manual/burp-suite-free-edition/noarch/tools/chocolateyInstall.ps1
+++ manual/burp-suite-free-edition/noarch/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $appDir = "${toolsDir}\app"
 $packageArgs = @{
   packageName  = 'burp-suite-free-edition'
   fileFullPath = "${appDir}\burpsuite.jar"
-  url          = 'https://portswigger.net/Burp/Releases/Download?productId=100&version=2021.7.2&type=jar'
-  checksum     = '6c26e881a1502e9936819da6ccbd37a83229e23dfe8b41b743f526adfde611f9cef6d4ee82a611b835ef44eed0da9c2dc564682d6b8c2b03d10940586eb6c373'
+  url          = 'https://portswigger.net/Burp/Releases/Download?productId=100&version=2021.8&type=jar'
+  checksum     = 'a5a122ca8d22929dc367e4570c021aa9331c19a7249d8579990e14b0355b9c97173ba43791d00ea84ff533a18683e653c848e91d227bfab59aa4b9ae0a10be79'
   checksumType = 'sha512'
 }
 Get-ChocolateyWebFile @packageArgs
@@ -14,7 +14,7 @@ $env:ChocolateyPackageInstallLocation = $appDir
 $menuPrograms = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutArgs = @{
   shortcutFilePath = "${menuPrograms}\Burp Suite Community Edition.lnk"
-  targetPath       = "${env:JAVA_HOME}\bin\javaw.exe"
+  targetPath       = '%JAVA_HOME%\bin\javaw.exe'
   arguments        = "-jar `"${appDir}\burpsuite.jar`""
   iconLocation     = "${toolsDir}\icon.ico"
 }
