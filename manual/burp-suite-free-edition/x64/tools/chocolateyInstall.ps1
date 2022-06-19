29cca38417906f1f56ae9c56955a3b1508d0e03d
Adam Gabrys <adam.gabrys@live.com>
2022-06-19 20:34:43 +0200

    update burp-suite-free-edition to 2022.3.5-beta

--- manual/burp-suite-free-edition/x64/tools/chocolateyInstall.ps1
+++ manual/burp-suite-free-edition/x64/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@ $packageArgs = @{
   packageName    = 'burp-suite-free-edition'
   fileType       = 'exe'
   silentArgs     = '-q'
-  url64bit       = 'https://portswigger-cdn.net/burp/releases/download?product=community&version=2022.3.4&type=WindowsX64'
-  checksum64     = '09ed28aafcefc92cd0733e99735743f67ffa0b564a89949f90dd1825c9366ee9142e3702a3edcc783eaf2f7be55dc0f7b17f9113350a70eab96d89c61ba45190'
+  url64bit       = 'https://portswigger-cdn.net/burp/releases/download?product=community&version=2022.3.5&type=WindowsX64'
+  checksum64     = 'b9d2f0135de7592dbd1f68ad5059d13bc536adad7cf057f9634202bb99546b670562a4f9efa69038b191500daa2da2910bb9937afa4bd1899d9fc4f59f0031e9'
   checksumType64 = 'sha512'
 }
 Install-ChocolateyPackage @packageArgs
