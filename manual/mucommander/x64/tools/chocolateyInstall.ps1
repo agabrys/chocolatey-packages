3b25088e89de660351481664b78772c0dc844b82
Adam Gabrys <adam.gabrys@live.com>
2024-10-06 20:48:10 +0200

    update mucommander to 1.5.0

--- manual/mucommander/x64/tools/chocolateyInstall.ps1
+++ manual/mucommander/x64/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@ $packageArgs = @{
   packageName    = 'mucommander'
   fileType       = 'msi'
   silentArgs     = '/quiet'
-  url64          = 'https://github.com/mucommander/mucommander/releases/download/1.4.1-1/mucommander-1.4.1.msi'
-  checksum64     = 'ebefb7da133a56ad6915f6173e525942f6dd21c3658593a4a6ceee7d1b306201839de4a776fc23bf7f401fa0e184f01ed732f82cd32605a85139f141e0c11d48'
+  url64          = 'https://github.com/mucommander/mucommander/releases/download/1.5.0-1/mucommander-1.5.0.msi'
+  checksum64     = '69f7e2d8c6e8668b0972cd29da3d88100c768c3f92ca25a27e838e88acfb77dbd64bf38977ac7549e977adf36bf85478a58ef7220267735eadffacd659e94c9c'
   checksumType64 = 'sha512'
 }
 Install-ChocolateyPackage @packageArgs
